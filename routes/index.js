810530492fabd9c08a304d80e008f00796c78d2e
Annie <haiau762@gmail.com>
2019-04-22 17:30:06 +0700

    add demo info product

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SoloQ Laptop Shop' });
 });
 
+/* GET detail product page. */
+router.get('/detail-product', function(req, res, next) {
+  res.render('product/product-detail', { title: 'Detail product' });
+});
+
 module.exports = router;
