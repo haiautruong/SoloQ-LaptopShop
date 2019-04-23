777df6b73934374a2ee3a65e9c03ac26ee968007
Annie <haiau762@gmail.com>
2019-04-23 22:15:20 +0700

    add admin feafutes

--- routes/adminController.js
+++ routes/adminController.js
@@ -5,8 +5,32 @@ router.get('/', function(req, res) {
   var vm = {
     title: 'Admin Home',
     layout: 'admin.handlebars'
-};  
+  };  
     res.render('admin/index', vm);
-  });
+});
+
+router.get('/order', function(req, res) {
+  var vm = {
+    title: 'Manage Order',
+    layout: 'admin.handlebars'
+  };  
+    res.render('admin/order', vm);
+});
+
+router.get('/user', function(req, res) {
+  var vm = {
+    title: 'Manage User',
+    layout: 'admin.handlebars'
+  };  
+    res.render('admin/user', vm);
+});
+
+router.get('/product', function(req, res) {
+  var vm = {
+    title: 'Manage Product',
+    layout: 'admin.handlebars'
+  };  
+    res.render('admin/product', vm);
+});
 
 module.exports = router;
