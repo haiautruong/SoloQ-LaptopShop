123bce1049caa32006722085b872c279ddc0c6fd
Annie <haiau762@gmail.com>
2019-05-26 23:26:34 +0700

    add model user

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema(
+    {
+        username: {type: String, required: true},
+        password: {type: String, required: true}
+    }
+)
+
+module.exports = user;
