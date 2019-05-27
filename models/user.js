224f5fb8cf2d706553fb7ea8e1674417aea237a3
Annie <haiau762@gmail.com>
2019-05-27 11:38:01 +0700

    update model user + layout login

--- models/user.js
+++ models/user.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const user = new mongoose.Schema(
     {
-        username: {type: String, required: true},
-        password: {type: String, required: true}
-    }
+        username: {type: String, required: true, lowercase: true, unique: true},
+        password: {type: String, required: true},
+        name: {type: String},
+        phone: {type: String, required: true}
+    },
+    
+    {timestamps: true}
 )
 
 module.exports = user;
